ef5b112cc0476923551ee234f6b3e392f9ca2bde
santimc <santimonserr@hotmail.com>
2020-01-07 12:07:31 -0300

    5 rouds whit user input

--- main_code.js
+++ main_code.js
@@ -1,6 +1,9 @@
 let element = ["Rock", "Paper", "Scissors", "Rock"];
+let computerScore = 0;
+let playerScore = 0;
+
 function computerPlay() {
-  const random = Math.floor(Math.random() * 3);  //gives a value between 0 and 2
+  let random = Math.floor(Math.random() * 3);  //gives a value between 0 and 2
   return random;
       //return ("unexpected error"; console.error("Random compueter selection");)
 }
@@ -11,21 +14,35 @@ function capitalize(string)
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = capitalize(playerSelection);
-  playerSelection = element.indexOf(playerSelection);
-  if (playerSelection != -1){
-    if (playerSelection == computerSelection){
-      return "Draw!";
-    } else if (element[playerSelection+1] == element[computerSelection]) {
-      return "Lose "+ element[computerSelection] + " defets " + element[playerSelection];
-    } else {
-      return "Win " + element[playerSelection] + " defets " + element[computerSelection];
-    }
+function game() {
+  let i = 0;
+  console.log("Computer Score: " + computerScore + " Player Score: " + playerScore);
+  while (i < 5) {
+    let playerSelection = prompt("Choose Wisely");
+    let computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+    console.log("Computer Score: " + computerScore + " Player Score: " + playerScore);
   }
-  return "Try tiping again";
+
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = capitalize(playerSelection);
+    playerSelection = element.indexOf(playerSelection);
+    if (playerSelection != -1){
+      if (playerSelection == computerSelection){
+        i++;
+        return console.log("Draw!");
+      } else if (element[playerSelection+1] == element[computerSelection]) {
+        computerScore ++;
+        i ++;
+        return console.log("You Lose "+ element[computerSelection] + " defets " + element[playerSelection]);
+      } else {
+        playerScore ++;
+        i++;
+        return console.log("You Win! " + element[playerSelection] + " defets " + element[computerSelection]);
+      }
+    }
+    return console.error("Try typing again");
+  }
 }
 
-let playerSelection = 'rock';
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+game();
