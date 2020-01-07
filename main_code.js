87cc7c915a7fdafcb64e8cae8545f77a5a430411
santimc <santimonserr@hotmail.com>
2020-01-07 11:18:42 -0300

    code for one round

--- main_code.js
+++ main_code.js
@@ -1,1 +1,31 @@
-alert("hola");
+let element = ["Rock", "Paper", "Scissors", "Rock"];
+function computerPlay() {
+  const random = Math.floor(Math.random() * 3);  //gives a value between 0 and 2
+  return random;
+      //return ("unexpected error"; console.error("Random compueter selection");)
+}
+
+function capitalize(string)
+{
+  string = string.toLowerCase();
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = capitalize(playerSelection);
+  playerSelection = element.indexOf(playerSelection);
+  if (playerSelection != -1){
+    if (playerSelection == computerSelection){
+      return "Draw!";
+    } else if (element[playerSelection+1] == element[computerSelection]) {
+      return "Lose "+ element[computerSelection] + " defets " + element[playerSelection];
+    } else {
+      return "Win " + element[playerSelection] + " defets " + element[computerSelection];
+    }
+  }
+  return "Try tiping again";
+}
+
+let playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
