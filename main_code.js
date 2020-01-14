2f700888a8acccfc970dd6114d8c62e07a29c901
santimc <santimonserr@hotmail.com>
2020-01-14 12:15:35 -0300

    Cmplete simple ui

--- main_code.js
+++ main_code.js
@@ -1,6 +1,15 @@
-let element = ["Rock", "Paper", "Scissors", "Rock"];
+const element = ["Rock", "Paper", "Scissors", "Rock"];
 let computerScore = 0;
 let playerScore = 0;
+const diplayPlayerScore = document.querySelector('#diplayPlayerScore');
+const diplayComputerScore = document.querySelector('#diplayComputerScore');
+const dispayComputerSelection = document.querySelector('#dispayComputerSelection')
+
+
+function dispayScore() {
+  diplayComputerScore.textContent = computerScore;
+  diplayPlayerScore.textContent = playerScore;
+}
 
 function computerPlay() {
   let random = Math.floor(Math.random() * 3);  //gives a value between 0 and 2
@@ -19,26 +28,29 @@ function capitalize(string){
     button.addEventListener('click', game);
   });
 
-  function playRound(playerSelection, computerSelection) {
-    playerSelection = capitalize(playerSelection);
-    playerSelection = element.indexOf(playerSelection);
-    if (playerSelection != -1){
-      if (playerSelection == computerSelection){
-        return console.log("Draw!");
-      } else if (element[playerSelection+1] == element[computerSelection]) {
-        computerScore ++;
-        return console.log("You Lose "+ element[computerSelection] + " defets " + element[playerSelection]);
-      } else {
-        playerScore ++;
-        return console.log("You Win! " + element[playerSelection] + " defets " + element[computerSelection]);
-      }
+function playRound(playerSelection, computerSelection) {
+  playerSelection = capitalize(playerSelection);
+  playerSelection = element.indexOf(playerSelection);
+  if (playerSelection != -1){
+    if (playerSelection == computerSelection){
+      return console.log("Draw!");
+    } else if (element[playerSelection+1] == element[computerSelection]) {
+      computerScore ++;
+      dispayScore();
+      return console.log("You Lose "+ element[computerSelection] + " defets " + element[playerSelection]);
+    } else {
+      playerScore ++;
+      dispayScore();
+      return console.log("You Win! " + element[playerSelection] + " defets " + element[computerSelection]);
     }
-    return console.error("Try typing again");
   }
+  return console.error("Try typing again");
+}
 
-function game(e){
+function game(e) {
   let playerSelection = e.target.id;
   let computerSelection = computerPlay();
+  dispayComputerSelection.textContent = element[computerSelection];
   playRound(playerSelection, computerSelection);
   const container = document.querySelector('#container');
   console.log("Computer Score: " + computerScore + " Player Score: " + playerScore);
